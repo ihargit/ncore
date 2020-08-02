2665c6291dc21f3c28ffdc938152e46a1e659a29
ihargit <ihar.kharashun@gmail.com>
2020-08-02 21:59:32 +0300

    feat: tasks

---
+++ basics/src/convert-csv.js
@@ -0,0 +1,25 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+const { Transform, pipeline } = require('stream');
+
+const csvFilePath = './csv/books.csv';
+
+const readStream = fs.createReadStream(csvFilePath);
+const writeStream = fs.createWriteStream('./converted-csv.txt');
+const convertStream = new Transform({
+  transform(chunk, encoding, callback) {
+    const object = JSON.parse(String(chunk));
+    const line = Object.keys(object).reduce((acc, key) => {
+      acc[key.toLowerCase()] = object[key];
+      return acc;
+    }, {});
+    delete line['amount'];
+    callback(null, `${JSON.stringify(line)}\n`);
+  },
+});
+
+pipeline(readStream, csv(), convertStream, writeStream, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
